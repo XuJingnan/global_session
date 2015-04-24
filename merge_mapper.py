1eae46c8d9a67a67b1a41c04997ce33dabfe3fd2
xujingnan <xujingnan308@163.com>
2015-04-24 14:44:22 +0800

    limit 500 records every udwid

--- merge_mapper.py
+++ merge_mapper.py
@@ -6,6 +6,10 @@ class MergeMapper(object):
 
     def __init__(self):
         self.field_sep = '\0'
+        self.count = 0
+        self.previous_key = ''
+        self.data_table_name = 'udwetl_global_session_data'
+        self.max_data_num = 500
 
     def init_value(self, line):
         line = line.strip()
@@ -17,6 +21,7 @@ class MergeMapper(object):
         if len(self.values) <= 2:
             self.stderr_out('init_value', 'wrong field number')
             return -2
+        self.tablename = self.values[0]
         self.key = self.values[1]  # map_key
         return 0
 
@@ -26,9 +31,20 @@ class MergeMapper(object):
             sys.exit(exit_code)
 
     def map(self):
-        self.values[0] += '_merge'  # table name
-        print '\t'.join([self.key, self.field_sep.join(self.values)])
-
+        if self.key != self.previous_key:
+            self.values[0] += '_merge'  # table name
+            print '\t'.join([self.key, self.field_sep.join(self.values)])
+            self.previous_key = self.key
+            if self.tablename == self.data_table_name:
+                self.count = 1
+        else:
+            if self.tablename == self.data_table_name and self.count == self.max_data_num:
+                return 
+            self.values[0] += '_merge'  # table name
+            print '\t'.join([self.key, self.field_sep.join(self.values)])
+            if self.tablename == self.data_table_name:
+                self.count += 1
+            
 mapper = MergeMapper()
 
 for line in sys.stdin:
